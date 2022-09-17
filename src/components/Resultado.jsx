71065b2f2a422e48b26f090e3b8deae693cd2aa7
henryposa <luisgasthof@hotmail.com>
2022-09-17 17:04:29 -0500

    añadiendo mas info al resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,11 +1,47 @@
 import useCotizador from "./hooks/useCotizador"
+// 2 import
+import { MARCAS, PLANES } from "./constants"
 
 const Resultado = () => {
-    const { resultado } = useCotizador()
+    // 1 extraer los datos 
+    const { resultado, datos } = useCotizador()
+    // 3
+    const { marca, plan, year } = datos
+
+    // 4 Filtra por el id no en base a la posición
+    const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
+    // 7
+    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan))
     // para quitar el 0 debajo 
     if (resultado === 0) return null
     return (
-        <div>{resultado}</div>
+        <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+            <h2 className="text-gray-600 font-black text-3xxl">
+                Resumen
+            </h2>
+            {/* my margen */}
+            <p className="my-2 ">
+                <span className="font-bold">Marca: </span>
+                {/* 5 */}
+                {nombreMarca.nombre}
+            </p>
+
+            {/* 6 */}
+            <p className="my-2 ">
+                <span className="font-bold">Plan: </span>
+                {nombrePlan.nombre}
+            </p>
+
+            <p className="my-2 ">
+                <span className="font-bold">Año: </span>
+                {year}
+            </p>
+
+            <p className="my-2 text-2xl ">
+                <span className="font-bold">Total cotización: </span>
+                {resultado}
+            </p>
+        </div>
     )
 }
 
