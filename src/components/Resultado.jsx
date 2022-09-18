5e82b022392ec89bc56fbede07ff497e1e5ecdf0
henryposa <luisgasthof@hotmail.com>
2022-09-18 18:24:02 -0500

    useCallback y useRef

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,17 +1,26 @@
+// 1
+import { useCallback, useMemo, useRef } from "react"
 import useCotizador from "./hooks/useCotizador"
-// 2 import
 import { MARCAS, PLANES } from "./constants"
 
 const Resultado = () => {
-    // 1 extraer los datos 
     const { resultado, datos } = useCotizador()
-    // 3
     const { marca, plan, year } = datos
+    // Filtra por el id, no en base a la posición
+    // 2 Queremos evitar el Re render en la marca y el plan
+    const [nombreMarca] = useCallback(
+        MARCAS.filter(m => m.id === Number(marca)),
+        [resultado])
+    const [nombrePlan] = useCallback(
+        PLANES.filter(p => p.id === Number(plan)),
+        [resultado])
+    // 3 congela el primer valor, o el escogido luego 
+    // de dar click en cotizar, así seleccionemos otros años, 
+    // los seleccionados se guardan en el estado y si cambian allí
+    const yearRef = useRef(year)
+    console.log("year:", year);
+    console.log("yearRef:", yearRef);
 
-    // 4 Filtra por el id no en base a la posición
-    const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
-    // 7
-    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan))
     // para quitar el 0 debajo 
     if (resultado === 0) return null
     return (
@@ -22,11 +31,9 @@ const Resultado = () => {
             {/* my margen */}
             <p className="my-2 ">
                 <span className="font-bold">Marca: </span>
-                {/* 5 */}
                 {nombreMarca.nombre}
             </p>
 
-            {/* 6 */}
             <p className="my-2 ">
                 <span className="font-bold">Plan: </span>
                 {nombrePlan.nombre}
@@ -34,7 +41,8 @@ const Resultado = () => {
 
             <p className="my-2 ">
                 <span className="font-bold">Año: </span>
-                {year}
+                {/* 4 */}
+                {yearRef.current}
             </p>
 
             <p className="my-2 text-2xl ">
